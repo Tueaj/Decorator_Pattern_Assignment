5e72c4d201934f7a2ff39678ba1d6ae3e574e530
AsgerHolm <47742247+AsgerHolm@users.noreply.github.com>
2020-11-03 11:46:25 +0100

    Extension upon MenuBugerDecorator

--- Restaurant_Decorator_Shop/Shop_Lib/Menu/MenuBurgerDecorator.cs
+++ Restaurant_Decorator_Shop/Shop_Lib/Menu/MenuBurgerDecorator.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Shop_Lib.Burger;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Shop_Lib.Menu
 {
-    class MenuBurgerDecorator
+    public class MenuBurgerDecorator : MenuDecorator
     {
+        private readonly IBurger burger;
+        public MenuBurgerDecorator(IMenu menu):base(menu)
+            {
+
+
+            }
+
+        public override double Price()
+        {
+            return base.Price() + burger.BurgerPrice();
+        }
+
+        public override string GetReceipt()
+        {
+
+            return base.GetReceipt() + burger.GetReceipt();
+        }
     }
 }
